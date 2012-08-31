fbb31fd42bf2da8aec1c890c789631c5bbfacd48
David Silver <dsilver@candidatemetrics.com>
2012-08-31 14:17:25 -0700

    setting up

--- store-after/app/models/product.rb
+++
@@ -1,4 +0,0 @@
-class Product < ActiveRecord::Base
-  belongs_to :category
-  scope :unreleased, where(:released_at => nil)
-end
--- store-before/app/controllers/products_controller.rb
+++
@@ -1,9 +0,0 @@
-class ProductsController < ApplicationController
-  def index
-    @products = Product.order("released_at desc")
-  end
-
-  def show
-    @product = Product.find(params[:id])
-  end
-end
